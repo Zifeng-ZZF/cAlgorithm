a2bfca07fd5faa41c15a721d9b338496eace3548
zhangzifeng <zhangzifeng@DOMAIN.SENSETIME.COM>
2020-01-19 14:34:42 +0800

    bubble & insert in c++

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 
-template <class T> int len(T& arr)
+void bubbleSort(int arr[], int length)
 {
-    return (int)sizeof(arr) / sizeof(arr[0]);
+    if (length <= 1)
+        return;
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
-void sort(int arr[], int low, int high)
+void insertSort(int arr[], int length)
+{
+    if (length <= 1)
+        return;
+        
+    for (int i = 1; i < length; i++)
+    {
+        // must compare starting from the last/newest one
+        for (int j = i; j > 0; j--) 
+        {
+            if(arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+}
+
+void quickSort(int arr[], int low, int high)
 {
     if ((high - low) <= 0)
     {
         return;
     }
-
     int i = low;
     int j = high;
     int marker = arr[high];
-
     while (i < j)
     {
         while (arr[i] <= marker && i < j) i++;
@@ -25,7 +57,6 @@ void sort(int arr[], int low, int high)
             arr[i] = arr[j];
             arr[j] = temp;
         }
-
         while (arr[j] >= marker && i < j) j--;
         if (i < j)
         {
@@ -34,16 +65,24 @@ void sort(int arr[], int low, int high)
             arr[i] = temp;
         }
     }
-
-    sort(arr, low, i - 1);
-    sort(arr, i + 1, high);
+    quickSort(arr, low, i - 1);
+    quickSort(arr, i + 1, high);
 }
 
 int main ()
 {
     int arr[10]{2, 3, 10, 1, 4, 7, 6, 9, 8, 5};
-    sort(arr, 0, len(arr) - 1);
-    for (int i = 0; i < len(arr); i++)
+
+    // cannot be called inside a function to return the length
+    // array as parameter is a reference which means it is an integer long
+    // it has to be calc in the area where it is defined
+    int len = (int)sizeof(arr) / sizeof(arr[0]);
+
+    // quickSort(arr, 0, len(arr) - 1);
+    // bubbleSort(arr, len);
+    insertSort(arr, len);
+
+    for (int i = 0; i < len; i++)
     {
         std::cout << arr[i] << " ";
     }
