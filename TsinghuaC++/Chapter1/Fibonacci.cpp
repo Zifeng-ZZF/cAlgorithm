49a6a70d59d4fdeba62036b679e4a63332e7192f
zhangzifeng <zhangzifeng@DOMAIN.SENSETIME.COM>
2020-03-31 11:30:38 +0800

    fibonacci classification refined

--- TsinghuaC++/Chapter1/Fibonacci.cpp
+++ TsinghuaC++/Chapter1/Fibonacci.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 //Find n the fibonacci number
 //fib = {1, 1, 2, 3, 5, 8, 13, 21, ...}
 
 // 1. binary recursion
-// O(2^n), call repeatedly same instances of the function
+// O(2^n), call repeatedly same instances of the function. Precisely O(1.6^n), golden ratio = 1.618...
 // not useful
 int fib1(int n)
 {
@@ -14,33 +15,27 @@ int fib1(int n)
     return fib1(n - 1) + fib1(n - 2);
 }
 
-// 2.1 tabulation 制表的方式: O(n), linear recursion
-int fib2(int n, int& prev)
+// 2.1 tabulation 制表的方式: O(n), linear recursion。这是 Bottle-Up 的方式
+int fib21(int n)
 {
-    if (n == 0)
-    {
-        prev = 1; // 前2项
-        return 0; // 前1项
-    }
-    // 计算前两项
-    int prevPrev;
-    prev = fib2(n - 1, prevPrev);
-    return prevPrev + prev;
+    std::vector<int> tab(n+1);
+    tab[0] = 0;
+    tab[1] = 1;
+    for (int i = 2; i <= n; ++i)
+        tab[i] = tab[i-1] + tab[i-2];
+    return tab[n];
 }
-// 2.2 memoization, 用数组记录 n > 1的项， 同样是O(n)
+// 2.2 memoization, 用数组记录 n > 1的项， 同样是O(n)。这是 Top-Down 的方式
 int fib22(int n, int mem[])
 {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    else if (!mem[n]) 
+    if (n <= 1)
+        mem[n] = n;
+    else
         mem[n] = fib22(n - 1, mem) + fib22(n - 2, mem);
     return mem[n];
 }
-
-// 3. Dynamic programming. Interation method: O(n) time and space
-int fib3(int n)
+// 2.3 Dynamic programming without using array. Interation method: O(n) time and space
+int fib23(int n)
 {
     int prev = 1;
     int ans = 0;
@@ -53,33 +48,20 @@ int fib3(int n)
     return ans;
 }
 
-// 3.1 Dynamic programming & memoization
-int fib31(int n)
-{
-    int dp[n];
-    dp[0] = 0;
-    dp[1] = 1;
-    int k = 2;
-    while (k <= n)
-    {
-        dp[k] = dp[k - 1] + dp[k - 2];
-        k++;
-    }
-    return dp[n];
-}
-
 int main()
 {
-    int const input = 7;
+    int const input = 8; // 1 1 2 3 5 8 13 21 34 55 89
     int output;
     int prev;
-    // output = fib1(7);
-    // output = fib2(input, prev);
-    // output = fib3(input);
     int mem[input + 1];
+    for (int i = 0; i < input + 1; ++i)
+    {
+        mem[i] = 0;
+    }
     // output = fib22(input, mem);
-    output = fib31(7);
+    output = fib21(input);
+    // output = fib31(7);
     std::cout << "The " << input << "th number of Fibonacci is " << output << std::endl;
-    std::cout << "The prev is " << prev << std::endl;
+    // std::cout << "The prev is " << prev << std::endl;
     return 0;
 }
